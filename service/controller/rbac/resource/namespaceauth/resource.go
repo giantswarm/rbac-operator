e55019c73c2e4a91aa29a7510973b0d3f7d80980
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-05-26 11:04:48 +0300

    Add controller code

---
+++ service/controller/rbac/resource/namespaceauth/resource.go
@@ -0,0 +1,150 @@
+package namespaceauth
+
+import (
+	"github.com/giantswarm/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	Name                = "namespaceauth"
+	tenantAdminRoleName = "tenant-admin"
+	viewAllRoleName     = "view-all"
+)
+
+type Config struct {
+	K8sClient k8sclient.Interface
+	Logger    micrologger.Logger
+
+	NamespaceAuth NamespaceAuth
+}
+
+type NamespaceAuth struct {
+	ViewAllTargetGroup     string
+	TenantAdminTargetGroup string
+}
+
+type Resource struct {
+	k8sClient kubernetes.Interface
+	logger    micrologger.Logger
+
+	namespaceAuth NamespaceAuth
+}
+
+type role struct {
+	name        string
+	verbs       []string
+	targetGroup string
+}
+
+func New(config Config) (*Resource, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if config.NamespaceAuth.ViewAllTargetGroup == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.NamespaceAuth.ViewAllTargetGroup must not be empty", config)
+	}
+	if config.NamespaceAuth.TenantAdminTargetGroup == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.NamespaceAuth.TenantAdminTargetGroup must not be empty", config)
+	}
+
+	r := &Resource{
+		k8sClient: config.K8sClient.K8sClient(),
+		logger:    config.Logger,
+
+		namespaceAuth: config.NamespaceAuth,
+	}
+
+	return r, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func appendUnique(slice []string, newElement string) []string {
+	for _, e := range slice {
+		if e == newElement {
+			return slice
+		}
+	}
+	return append(slice, newElement)
+}
+
+func newInternalRole(name, targetGroup string, verbs []string) role {
+	return role{
+		name:        name,
+		targetGroup: targetGroup,
+		verbs:       verbs,
+	}
+}
+
+func newRole(name string, resources []*metav1.APIResourceList, verbs []string) (*rbacv1.Role, error) {
+	var resourceNamesNamespace, apiGroupsNamespace []string
+	{
+		for _, resource := range resources {
+
+			gv, err := schema.ParseGroupVersion(resource.GroupVersion)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			apiGroupsNamespace = appendUnique(apiGroupsNamespace, gv.Group)
+
+			for _, apiResource := range resource.APIResources {
+				if apiResource.Namespaced {
+					resourceNamesNamespace = appendUnique(resourceNamesNamespace, apiResource.Name)
+				}
+			}
+		}
+	}
+
+	role := &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Rules: []rbacv1.PolicyRule{
+			rbacv1.PolicyRule{
+				APIGroups: apiGroupsNamespace,
+				Resources: resourceNamesNamespace,
+				Verbs:     verbs,
+			},
+		},
+	}
+
+	return role, nil
+}
+
+func newRoleBinding(name, targetGroupName string) *rbacv1.RoleBinding {
+	roleBinding := &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Subjects: []rbacv1.Subject{
+			rbacv1.Subject{
+				Kind: "Group",
+				Name: targetGroupName,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     name,
+		},
+	}
+
+	return roleBinding
+}
