6be7ca14adb3177e626122b2b1af815f30315695
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-05-29 14:23:23 +0300

    Release v0.4.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.3.3"
+	bundleVersion = "0.4.0"
 	description   = "The rbac-operator manages tenant namespace access in control-plane Kubernetes."
 	gitSHA        = "n/a"
 	name          = "rbac-operator"
 	source        = "https://github.com/giantswarm/rbac-operator"
-	version       = "0.3.3"
+	version       = "0.4.0"
 )
 
 func BundleVersion() string {
