90ec6510761e60e47f147fd753fcd7eb91c384af
GitHub Action <action@github.com>
2021-03-30 06:23:29 +0000

    Bump version to 0.12.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "rbac-operator"
 	source      = "https://github.com/giantswarm/rbac-operator"
-	version     = "0.12.0"
+	version     = "0.12.1-dev"
 )
 
 func Description() string {
