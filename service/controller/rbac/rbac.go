ef7b338698c2fe44aa97f2071cdcaa7cbc9954e6
Marian Steinbach <marian@giantswarm.io>
2021-12-02 15:59:50 +0100

    Fix comment

--- service/controller/rbac/rbac.go
+++ service/controller/rbac/rbac.go
@@ -57,7 +57,7 @@ func NewRBAC(config RBACConfig) (*RBAC, error) {
 			Selector:  selector,
 
 			// Name is used to compute finalizer names. This here results in something
-			// like operatorkit.giantswarm.io/rbac-operator-RBAC-controller.
+			// like operatorkit.giantswarm.io/rbac-operator-rbac-controller.
 			Name: project.Name() + "-rbac-controller",
 		}
 
