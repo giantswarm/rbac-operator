36707810566388396447be37be8af9e421498301
Paweł Kopiczko <kopiczko@gmail.com>
2021-12-09 09:34:34 +0000

    Add comment

--- service/controller/rbac/selector.go
+++ service/controller/rbac/selector.go
@@ -4,6 +4,13 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 )
 
+// wrongSelector is named like that because it only partially implements
+// labels.Selector interface and relies on the fact that operatorkit/v5 calls
+// only `Matches` method. This feels wrong. Hence the name. If you want to fix
+// it then you are welcome to do so. If the underlying implementation changes
+// this will result in a panic. This is to make it work with operatorkit/v5.
+// Kubernetes in general doesn't support label selector with OR condition and
+// we need that here.
 type wrongSelector struct {
 	labels.Selector
 	matchesFn func(labels.Labels) bool
