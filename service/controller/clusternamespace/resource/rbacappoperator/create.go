e064854d9992ae6f89f16cb8d25fa70d03146a9b
Laszlo Uveges <laszlo@giantswarm.io>
2022-05-06 00:15:39 +0200

    Add dummy base for creating cluster roles for app-operator

--- service/controller/clusternamespace/resource/rbacappoperator/create.go
+++ service/controller/clusternamespace/resource/rbacappoperator/create.go
@@ -28,6 +28,12 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		"message", fmt.Sprintf("Reconciling cluster namespace: %s.", cl.Name),
 	)
 
+	// Allow working with some generic resources across namespaces
+	err = r.CreateClusterRoleAndBinding(ctx, cl)
+	if err != nil {
+		return err
+	}
+
 	// Allow getting catalog configmaps in giantswarm namespace
 	err = r.CreateCatalogReaderRoleAndBinding(ctx, cl)
 	if err != nil {
@@ -38,6 +44,96 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	err = r.CreateOwnNamespaceRoleAndBinding(ctx, cl)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (r *Resource) CreateClusterRoleAndBinding(ctx context.Context, cl corev1.Namespace) error {
+	var clusterRole = &rbacv1.ClusterRole{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRole",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("app-operator-%s", cl.Name),
+			Labels: map[string]string{
+				label.ManagedBy: project.Name(),
+			},
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"apps"},
+				Verbs:     []string{"get", "list", "update", "patch", "watch"},
+			},
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"apps/status"},
+				Verbs:     []string{"create", "patch", "update"},
+			},
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"catalogs"},
+				Verbs:     []string{"get", "list", "patch", "watch"},
+			},
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"appcatalogs"},
+				Verbs:     []string{"create", "delete", "get", "list", "patch", "update", "watch"},
+			},
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"appcatalogs/status"},
+				Verbs:     []string{"create", "patch", "update"},
+			},
+			{
+				APIGroups: []string{"application.giantswarm.io"},
+				Resources: []string{"appcatalogentries"},
+				Verbs:     []string{"*"},
+			},
+		},
+	}
+
+	// TODO clusternamespaceresources already has logic to create / update / delete roles and cluster roles better
+	_, err := r.k8sClient.K8sClient().RbacV1().ClusterRoles().Create(ctx, clusterRole, metav1.CreateOptions{})
+	if apierrors.IsAlreadyExists(err) {
+		// do nothing
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
+	clusterRoleBinding := &rbacv1.ClusterRoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRole",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("app-operator-%s", cl.Name),
+			Labels: map[string]string{
+				label.ManagedBy: project.Name(),
+			},
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      fmt.Sprintf("app-operator-%s", cl.Name),
+				Namespace: cl.Name,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     clusterRole.Name,
+		},
+	}
+
+	// TODO clusternamespaceresources already has logic to create / update / delete roles and cluster roles better
+	_, err = r.k8sClient.K8sClient().RbacV1().ClusterRoleBindings().Create(ctx, clusterRoleBinding, metav1.CreateOptions{})
+	if apierrors.IsAlreadyExists(err) {
+		// do nothing
+	} else if err != nil {
+		return microerror.Mask(err)
 	}
 
 	return nil
