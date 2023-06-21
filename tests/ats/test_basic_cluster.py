6ae0ec652601dbf031975c2be3e52ee61aab2ba9
Moritz Gottschling <moritz@giantswarm.io>
2023-06-21 14:24:27 +0200

    Create test for "externalresources" resource for controller "rbac"

--- tests/ats/test_basic_cluster.py
+++ tests/ats/test_basic_cluster.py
@@ -1,15 +1,19 @@
 from typing import List
+import logging
 
 import pykube
 import pytest
 from pytest_helm_charts.clusters import Cluster
 from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
+import pytest_helm_charts.k8s.namespace as pytest_namespace
 
 
-timeout: int = 60
+LOGGER = logging.getLogger(__name__)
 
-deployment_names = ["rbac-operator"]
-namespace_name = "default"
+TIMEOUT: int = 60
+
+DEPLOYMENT_NAMES = ["rbac-operator"]
+NAMESPACE_NAME = "default"
 
 
 @pytest.mark.smoke
@@ -22,12 +26,11 @@ def test_api_working(kube_cluster: Cluster) -> None:
 def app_deployments(kube_cluster: Cluster) -> List[pykube.Deployment]:
     if kube_cluster.kube_client is None:
         raise Exception("kube_client is None")
-    print(pykube.Deployment.objects(kube_cluster.kube_client))
     deployments = wait_for_deployments_to_run(
         kube_cluster.kube_client,
-        deployment_names,
-        namespace_name,
-        timeout,
+        DEPLOYMENT_NAMES,
+        NAMESPACE_NAME,
+        TIMEOUT,
     )
     return deployments
 
@@ -40,3 +43,55 @@ def test_pods_available(
 ):
     for d in app_deployments:
         assert int(d.obj["status"]["readyReplicas"]) > 0
+
+
+@pytest.mark.smoke
+@pytest.mark.flaky(reruns=5, reruns_delay=10)
+def test_rbac_controller_external_resources(kube_cluster: Cluster):
+    LOGGER.info("Creating org and cluster namespaces")
+    cluster_namespace_name = "r9b5q"
+    org_name = "test"
+    org_namespace_name = f"org-{org_name}"
+
+    expected_cluster_role_binding_names = [
+        f"organization-organization-{org_name}-read",
+        f"releases-organization-{org_name}-read",
+    ]
+
+    expected_cluster_role_names = [
+        "read-releases",
+        f"organization-{org_name}-read",
+    ]
+
+    # expected_cluster_role_names = [
+    #     f"organization-{cluster_namespace_name}-read",
+
+    kube_client = kube_cluster.kube_client
+    if kube_client is None:
+        raise Exception("kube_client should be set")
+
+    LOGGER.info("Creating org and cluster namespaces")
+    pytest_namespace.ensure_namespace_exists(
+        kube_client,
+        org_namespace_name,
+        extra_metadata={"labels": {"giantswarm.io/organization": org_namespace_name}},
+    )
+
+    pytest_namespace.ensure_namespace_exists(
+        kube_client,
+        cluster_namespace_name,
+        extra_metadata={
+            "labels": {
+                "giantswarm.io/organization": org_namespace_name,
+                "giantswarm.io/cluster": cluster_namespace_name,
+            }
+        },
+    )
+
+    # raises if not found
+    for expected_cluster_role_name in expected_cluster_role_binding_names:
+        pykube.ClusterRoleBinding.objects(kube_client).get(
+            name=expected_cluster_role_name
+        )
+    for expected_cluster_role_name in expected_cluster_role_names:
+        pykube.ClusterRole.objects(kube_client).get(name=expected_cluster_role_name)
