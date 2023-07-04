06e6cb635abb6afee384f93edef23dd1b207c1e2
taylorbot <dev@giantswarm.io>
2023-07-04 12:45:04 +0000

    Bump version to 0.34.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "rbac-operator"
 	source      = "https://github.com/giantswarm/rbac-operator"
-	version     = "0.34.3"
+	version     = "0.34.4-dev"
 )
 
 func Description() string {
