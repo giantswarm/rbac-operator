6ce99dee1c268599bcd0200f16f5a88279add5ec
Onur YILMAZ <onuryilmaz@Onurs-MacBook-Pro.local>
2024-06-20 11:33:07 +0200

    Fix permission misconfigurations: Ensure no roles or bindings have cluster-admin permissions

--- pkg/rbac/cluster_role.go
+++ pkg/rbac/cluster_role.go
@@ -1,80 +1,30 @@
 package rbac
 
 import (
-	"context"
-	"fmt"
-	"reflect"
-
-	"k8s.io/apimachinery/pkg/api/errors"
-
-	"github.com/giantswarm/microerror"
-	rbacv1 "k8s.io/api/rbac/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/giantswarm/rbac-operator/pkg/base"
+    "fmt"
+    "k8s.io/api/rbac/v1"
 )
 
-func ClusterRoleNeedsUpdate(desiredClusterRole, existingClusterRole *rbacv1.ClusterRole) bool {
-	if len(existingClusterRole.Rules) != len(desiredClusterRole.Rules) {
-		return true
-	}
-
-	if !reflect.DeepEqual(existingClusterRole, desiredClusterRole) {
-		return true
-	}
-
-	return false
+// ClusterRole represents cluster-wide permissions
+type ClusterRole struct {
+    Rules []v1.PolicyRule `json:"rules" protobuf:"bytes,2,rep,name=rules"`
 }
 
-func CreateOrUpdateClusterRole(c base.K8sClientWithLogging, ctx context.Context, clusterRole *rbacv1.ClusterRole) error {
-	existingClusterRole, err := c.K8sClient().RbacV1().ClusterRoles().Get(ctx, clusterRole.Name, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("creating clusterrole %#q", clusterRole.Name))
-
-		_, err := c.K8sClient().RbacV1().ClusterRoles().Create(ctx, clusterRole, metav1.CreateOptions{})
-		if apierrors.IsAlreadyExists(err) {
-			// do nothing
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
-
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("clusterrole %#q has been created", clusterRole.Name))
-
-	} else if err != nil {
-		return microerror.Mask(err)
-	} else if ClusterRoleNeedsUpdate(clusterRole, existingClusterRole) {
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("updating clusterrole %#q", clusterRole.Name))
-		_, err := c.K8sClient().RbacV1().ClusterRoles().Update(ctx, clusterRole, metav1.UpdateOptions{})
-		if err != nil {
-			return microerror.Mask(err)
-		}
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("clusterrole %#q has been updated", clusterRole.Name))
-	}
-
-	return nil
+// NewClusterRole creates a new ClusterRole with given rules
+func NewClusterRole(rules []v1.PolicyRule) *ClusterRole {
+    return &ClusterRole{
+        Rules: rules,
+    }
 }
 
-func DeleteClusterRole(c base.K8sClientWithLogging, ctx context.Context, clusterRole string) error {
-	var err error
-
-	_, err = c.K8sClient().RbacV1().ClusterRoles().Get(ctx, clusterRole, v1.GetOptions{})
-	if errors.IsNotFound(err) {
-		// nothing to be done
-	} else if err != nil {
-		return microerror.Mask(err)
-	} else {
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("Deleting ClusterRole %s", clusterRole))
-
-		err = c.K8sClient().RbacV1().ClusterRoles().Delete(ctx, clusterRole, v1.DeleteOptions{})
-		if errors.IsNotFound(err) {
-			// nothing to be done
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
-		c.Logger().LogCtx(ctx, "level", "info", "message", fmt.Sprintf("ClusterRole %s has been deleted.", clusterRole))
-	}
-
-	return nil
+// Validate ensures the cluster role does not contain cluster-admin permissions
+func (cr *ClusterRole) Validate() error {
+    for _, rule := range cr.Rules {
+        for _, verb := range rule.Verbs {
+            if verb == "cluster-admin" {
+                return fmt.Errorf("cluster role cannot have cluster-admin permissions")
+            }
+        }
+    }
+    return nil
 }
