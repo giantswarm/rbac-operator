bac51480afa47c1ceef73a0f228cdd6402e603cf
Spyros Synodinos <spyros@giantswarm.io>
2024-08-06 17:37:39 +0300

    tests update

--- tests/ats/test_rbac_controller_externalresources.py
+++ tests/ats/test_rbac_controller_externalresources.py
@@ -1,12 +1,11 @@
 import logging
-import time
 from typing import Tuple
 
 import pykube
 import pytest
-from pytest_helm_charts.clusters import Cluster
+from pytest_helm_charts.fixtures import Cluster
 import pytest_helm_charts.k8s.namespace as pytest_namespace
-from decorators import retry
+from pytest_helm_charts.k8s.wait_for import wait_for_objects_to_appear, wait_for_objects_to_disappear
 
 LOGGER = logging.getLogger(__name__)
 
@@ -27,81 +26,67 @@ EXPECTED_CLUSTER_ROLE_NAMES = [
 
 @pytest.mark.smoke
 class TestRBACControllerExternalResources:
-    kube_client: pykube.HTTPClient
-
-    def init(self, kube_cluster: Cluster):
-        if kube_cluster.kube_client is None:
-            raise Exception("kube_client should be set")
-        self.kube_client = kube_cluster.kube_client
 
     @pytest.mark.smoke
+    @pytest.mark.flaky(reruns=3, reruns_delay=10)
     def test_rbac_controller_external_resources(self, kube_cluster: Cluster):
-        self.init(kube_cluster)
+        org_namespace, cluster_namespace = self.create_namespaces(kube_cluster.kube_client)
+        self.check_created(kube_cluster.kube_client)
+        self.delete_namespaces(kube_cluster.kube_client, cluster_namespace, org_namespace)
+        self.check_deleted(kube_cluster.kube_client)
 
-        org_namespace, cluster_namespace = self.create_namespaces()
-        self.check_created()
-
-        self.delete_namespaces(cluster_namespace, org_namespace)
-        self.check_deleted()
-
-    def create_namespaces(self) -> Tuple[pykube.Namespace, pykube.Namespace]:
+    def create_namespaces(self, kube_client) -> Tuple[pykube.Namespace, pykube.Namespace]:
         LOGGER.info("Creating org and cluster namespaces")
-        org_namespace, _ = pytest_namespace.ensure_namespace_exists(
-            self.kube_client,
+        org_namespace = pytest_namespace.create_namespace(
+            kube_client,
             ORG_NAMESPACE_NAME,
-            extra_metadata={
-                "labels": {"giantswarm.io/organization": ORG_NAMESPACE_NAME}
-            },
+            labels={"giantswarm.io/organization": ORG_NAMESPACE_NAME}
         )
-
-        cluster_namespace, _ = pytest_namespace.ensure_namespace_exists(
-            self.kube_client,
+        cluster_namespace = pytest_namespace.create_namespace(
+            kube_client,
             CLUSTER_NAMESPACE_NAME,
-            extra_metadata={
-                "labels": {
-                    "giantswarm.io/organization": ORG_NAMESPACE_NAME,
-                    "giantswarm.io/cluster": CLUSTER_NAMESPACE_NAME,
-                }
-            },
+            labels={
+                "giantswarm.io/organization": ORG_NAMESPACE_NAME,
+                "giantswarm.io/cluster": CLUSTER_NAMESPACE_NAME,
+            }
         )
         LOGGER.info("Created org and cluster namespaces")
-
         return org_namespace, cluster_namespace
 
-    @retry(max_retries=10)
-    def check_created(self):
+    def check_created(self, kube_client):
         LOGGER.info("Checking for expected cluster role bindings and roles")
-        # raises if not found
-        for expected_cluster_role_name in EXPECTED_CLUSTER_ROLE_BINDING_NAMES:
-            pykube.ClusterRoleBinding.objects(self.kube_client).get(
-                name=expected_cluster_role_name
-            )
-        for expected_cluster_role_name in EXPECTED_CLUSTER_ROLE_NAMES:
-            pykube.ClusterRole.objects(self.kube_client).get(
-                name=expected_cluster_role_name
-            )
+        wait_for_objects_to_appear(
+            kube_client,
+            EXPECTED_CLUSTER_ROLE_BINDING_NAMES,
+            pykube.ClusterRoleBinding,
+            timeout_seconds=60,
+        )
+        wait_for_objects_to_appear(
+            kube_client,
+            EXPECTED_CLUSTER_ROLE_NAMES,
+            pykube.ClusterRole,
+            timeout_seconds=60,
+        )
         LOGGER.info("Found expected cluster role bindings and roles")
 
-    def delete_namespaces(
-        self, cluster_namespace: pykube.Namespace, org_namespace: pykube.Namespace
-    ):
+    def delete_namespaces(self, kube_client, cluster_namespace: pykube.Namespace, org_namespace: pykube.Namespace):
         LOGGER.info("Deleting org and cluster namespaces")
-        org_namespace.delete()
-        cluster_namespace.delete()
+        pytest_namespace.delete_namespace(kube_client, cluster_namespace.name)
+        pytest_namespace.delete_namespace(kube_client, org_namespace.name)
         LOGGER.info("Deleted org and cluster namespaces")
 
-    @retry(max_retries=20)
-    def check_deleted(self):
-        try:
-            for expected_cluster_role_name in EXPECTED_CLUSTER_ROLE_BINDING_NAMES:
-                pykube.ClusterRoleBinding.objects(self.kube_client).get(
-                    name=expected_cluster_role_name
-                )
-            for expected_cluster_role_name in EXPECTED_CLUSTER_ROLE_NAMES:
-                pykube.ClusterRole.objects(self.kube_client).get(
-                    name=expected_cluster_role_name
-                )
-            time.sleep(5)
-            raise Exception("Cluster role bindings and roles still exist")
-        except pykube.exceptions.ObjectDoesNotExist:
-            LOGGER.info("Cluster role bindings and roles deleted")
+    def check_deleted(self, kube_client):
+        LOGGER.info("Checking for deletion of cluster role bindings and roles")
+        wait_for_objects_to_disappear(
+            kube_client,
+            EXPECTED_CLUSTER_ROLE_BINDING_NAMES,
+            pykube.ClusterRoleBinding,
+            timeout_seconds=120,
+        )
+        wait_for_objects_to_disappear(
+            kube_client,
+            EXPECTED_CLUSTER_ROLE_NAMES,
+            pykube.ClusterRole,
+            timeout_seconds=120,
+        )
+        LOGGER.info("Confirmed deletion of cluster role bindings and roles")
