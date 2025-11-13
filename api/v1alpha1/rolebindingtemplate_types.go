2168021496150c337dbd43396c562ff50b274bfe
Mirza Esaaf Shuja <mirzaesaaf@gmail.com>
2025-11-13 19:34:49 +0100

    Initialize RoleBindingTemplate controller

---
+++ api/v1alpha1/rolebindingtemplate_types.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RoleBindingTemplateSpec defines the desired state of RoleBindingTemplate
+type RoleBindingTemplateSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// The following markers will use OpenAPI v3 schema to validate the value
+	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+
+	Template RoleBindingTemplateResource `json:"template"`
+	Scopes   RoleBindingTemplateScopes   `json:"scopes"`
+}
+
+// RoleBindingTemplateResource describes the data needed to create a rolebinding from a template.
+type RoleBindingTemplateResource struct {
+	// Standard object's metadata.
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Subjects holds references to the objects the role applies to.
+	// +optional
+	Subjects []rbacv1.Subject `json:"subjects,omitempty"`
+
+	// RoleRef can reference a Role in the current namespace or a ClusterRole in the global namespace.
+	// If the RoleRef cannot be resolved, the Authorizer must return an error.
+	RoleRef rbacv1.RoleRef `json:"roleRef"`
+}
+
+// RoleBindingTemplateScopes describes the scopes the RoleBindingTemplate should be applied to
+type RoleBindingTemplateScopes struct {
+	OrganizationSelector metav1.LabelSelector `json:"organizationSelector"`
+}
+
+// RoleBindingTemplateStatus defines the observed state of RoleBindingTemplate.
+type RoleBindingTemplateStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the RoleBindingTemplate resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+
+// RoleBindingTemplate is the Schema for the rolebindingtemplates API
+type RoleBindingTemplate struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty,omitzero"`
+
+	// spec defines the desired state of RoleBindingTemplate
+	// +required
+	Spec RoleBindingTemplateSpec `json:"spec"`
+
+	// status defines the observed state of RoleBindingTemplate
+	// +optional
+	Status RoleBindingTemplateStatus `json:"status,omitempty,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// RoleBindingTemplateList contains a list of RoleBindingTemplate
+type RoleBindingTemplateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RoleBindingTemplate `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RoleBindingTemplate{}, &RoleBindingTemplateList{})
+}
