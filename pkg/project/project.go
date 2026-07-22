58d60b5aed1a87af83401b613b6ab8da59cb559e
taylorbot <dev@giantswarm.io>
2026-07-22 07:41:50 +0000

    chore: bump version to 1.0.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "rbac-operator"
 	source      = "https://github.com/giantswarm/rbac-operator"
-	version     = "1.0.0"
+	version     = "1.0.1-dev"
 )
 
 func Description() string {
