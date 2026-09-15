28d5e4207cc604323b0c612b860f4eee137f5609
taylorbot <dev@giantswarm.io>
2026-09-15 09:46:41 +0000

    chore(release): v1.2.0
    Release-Workflow-Run: 34954336500

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "rbac-operator"
 	source      = "https://github.com/giantswarm/rbac-operator"
-	version     = "1.1.1-dev"
+	version     = "1.2.0"
 )
 
 func Description() string {
